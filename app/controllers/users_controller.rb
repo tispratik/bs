8d9b7e3ca3bcb787f1847e18f1890f9fda50ac73
Pratik <tispratik@gmail.com>
2010-05-15 22:58:54 -0500

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,12 @@ class UsersController < ApplicationController
   before_filter :find_project, :except => [:me, :show]
   
   def show
-    @user = User.find_by_username(params[:id])
+    redirect_to :action => :me
   end
   
   def me
     @user = current_user
-    
-    sub_qry = sort_order('descend_by_created_at')
-    sub_qry = sub_qry + ".all(:include => [:assignee, :initiator, :updator, :project, :priorityDecode])"
-    @my_tasks = eval('Task.my' + sub_qry + ".paginate(:page => #{params[:upper] || 1}, :per_page => 10)")
-    @my_related = eval('Task.my_related' + sub_qry + ".paginate(:page => #{params[:lower] || 1}, :per_page => 10)")
-    
+    sub_qry
     render :show
   end
   
@@ -29,4 +24,10 @@ class UsersController < ApplicationController
   #    qry = qry + ".paginate(:page => #{params[:page] || 1}, :per_page => 8)"
   #  end
   
+  def sub_qry
+    sub_qry = sort_order('descend_by_created_at')
+    sub_qry = sub_qry + ".all(:include => [:assignee, :initiator, :updator, :project, :priorityDecode])"
+    @my_tasks = eval('Task.my' + sub_qry + ".paginate(:page => #{params[:upper] || 1}, :per_page => 10)")
+    @my_related = eval('Task.my_related' + sub_qry + ".paginate(:page => #{params[:lower] || 1}, :per_page => 10)") 
+  end
 end
